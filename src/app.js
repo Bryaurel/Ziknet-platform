4a0e0ea5c4711dd02d569a75bf6f4a954cb482b8
Bryan BAKONGO <b.bakongobw@alustudent.com>
2025-07-19 21:44:20 +0200

    update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config(); // Charge les variables d'environnement
+dotenv.config();
 
 const app = express();
 
@@ -14,8 +14,8 @@ app.use(express.json());
 const authRoutes = require("./routes/auth");
 const profileRoutes = require("./routes/profile");
 
-app.use("/auth", authRoutes);
-app.use("/profile", profileRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
 
 // Test route
 app.get("/", (req, res) => {
