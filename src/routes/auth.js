ac5a853f6c3497b525eba61384b88375e5ca1d43
Bryan BAKONGO <b.bakongobw@alustudent.com>
2025-07-19 21:44:20 +0200

    update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -109,4 +109,19 @@ router.post("/login", async (req, res) => {
   }
 });
 
+//Get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find().select("-password"); // Exclude passwords
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Test
+router.get("/test", (req, res) => {
+  res.send("Auth route is working");
+});
+
 module.exports = router;
